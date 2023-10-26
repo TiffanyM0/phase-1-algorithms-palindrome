f4bac55d11f8993be7b6d63b015f585fafaebf8e
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-26 14:58:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let result = stringCheck(word);
+  if (result == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function stringCheck(word) {
+  let back = word.length - 1;
+  for (let i = 0; i < back; i++) {
+    let x = word[i];
+    let y = word[back - i];
+    if (x != y) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  write a function that 
+  iterate over the string,front and backwards
+  boolean check for the both
 */
 
 /*
   Add written explanation of your solution here
+  the function stringCheck iterates over the characters
+  in the word argument; from left to right and right to left
+  it then checks if the values dont match with != to
+  return false. if they match it exits loop and returns true;
+
+  then i assign stringCheck to result in fun isPalindrome() then
+  boolean check for true or false.
 */
 
 // You can run `node index.js` to view these console logs
